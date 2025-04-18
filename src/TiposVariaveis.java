ba9baa7b31259bd1b4f99f711b804cf34a7579e8
alexandre-soares-93 <asoaresdeoliveira977@gmail.com>
2025-04-17 21:41:53 -0300

    Explicacao sobre constantes e variaveis, iniciacao a strings

--- src/TiposVariaveis.java
+++ src/TiposVariaveis.java
@@ -1,9 +1,23 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
+        //tipos primitivos
+        //estudem a classe String que representa texto na aplicação
+
+        String meuNome = "Alexandre Soares de Oliveira";
+
         double salarioMinimo = 2500.33;
 
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
         short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double PI = 3.14;
+
     }
 }
