9a381107643c53ae9d2660bf9765db6fac995742
alexandre-soares-93 <asoaresdeoliveira977@gmail.com>
2025-04-18 18:12:28 -0300

    operadores unários de decremento, incremento e negacao

--- src/Operadores.java
+++ src/Operadores.java
@@ -1,14 +1,30 @@
 public class Operadores {
 
     public static void main(String[] args) {
+        //x repeticao
+
         int numero = 5;
 
-        numero = - numero;
-        
+        numero++;
+        // numero + 1
+
+        System.out.println( ++ numero);
+
         System.out.println(numero);
 
-        numero = numero * -1;
+        System.out.println(numero -- );
 
         System.out.println(numero);
+
+        boolean variavel = true;
+
+        System.out.println(!variavel);
+        System.out.println(variavel);
+
+        variavel = !variavel;
+
+        System.out.println(variavel);
+        System.out.println(!variavel);
+
     }
 }
