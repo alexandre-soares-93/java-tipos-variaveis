1aa54f521ec23b4378b46dc7a9ad1cfffeefa5ed
alexandre-soares-93 <asoaresdeoliveira977@gmail.com>
2025-04-19 18:45:12 -0300

    Aula sobre operadores lógicos

--- src/Operadores.java
+++ src/Operadores.java
@@ -1,30 +1,21 @@
 public class Operadores {
 
     public static void main(String[] args) {
-        String nomeUm = "Eliud";
-        String nomeDois = new String("Eliud");
+        boolean condicao1 = true;
 
-        System.out.println(nomeUm.equals(nomeDois));
+        boolean condicao2 = true;
 
-
-        int numero1 = 1;
-        int numero2 = 2;
-
-        boolean simNao = numero1 == numero2;
-
-        if(numero1 < numero2) {
-            System.out.println("A nossa condição é verdadeira");
+        if(condicao1 && (7 > 4)) {
+            System.out.println("As duas condições são verdadeiras");
+        
         }
 
-        System.out.println("Numero 1 é igual a Numero 2 ?: " + simNao);
+        if(condicao1 || condicao2) {
+            System.out.println("Uma das condições é verdadeira");
+        
+        }
 
-        simNao = numero1 != numero2;
-
-        System.out.println("Numero 1 é diferente de Numero 2 ?: " + simNao);
-
-        simNao = numero1 > numero2;
-
-        System.out.println("Numero 1 é maior que Numero 2 ?: " + simNao);
+        System.out.println("Fim");
 
     }
 }
