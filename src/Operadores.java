4a865f2d0e47d1b2902783a8228e0dae0239669d
alexandre-soares-93 <asoaresdeoliveira977@gmail.com>
2025-04-19 18:18:51 -0300

    Aula sobre Operadores Relacionais

--- src/Operadores.java
+++ src/Operadores.java
@@ -1,15 +1,30 @@
 public class Operadores {
 
     public static void main(String[] args) {
+        String nomeUm = "Eliud";
+        String nomeDois = new String("Eliud");
 
-        int a, b;
+        System.out.println(nomeUm.equals(nomeDois));
 
-        a = 6;
-        b = 6;
 
-        String resultado = a == b ?"Verdadeiro" : "Falso"; // Expressão booleana
+        int numero1 = 1;
+        int numero2 = 2;
 
-        System.out.println(resultado);
+        boolean simNao = numero1 == numero2;
+
+        if(numero1 < numero2) {
+            System.out.println("A nossa condição é verdadeira");
+        }
+
+        System.out.println("Numero 1 é igual a Numero 2 ?: " + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("Numero 1 é diferente de Numero 2 ?: " + simNao);
+
+        simNao = numero1 > numero2;
+
+        System.out.println("Numero 1 é maior que Numero 2 ?: " + simNao);
 
     }
 }
